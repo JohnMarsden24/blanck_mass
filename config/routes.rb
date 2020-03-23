db3f10a44fb29b50e939b5f5f5321d6bfebe0272
John Marsden <john.marsden24@gmail.com>
2020-03-23 11:49:19 +0000

    added admin rights to user

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ Rails.application.routes.draw do
 
   get "/home", to: "pages#home"
 
-
-  # devise_for :users, path: '', path_names:
-  # {:sign_in => 'admin_login', :sign_out => 'admin_logout'}
-
-  # devise_for :users, :skip => [:registrations]
-
   devise_for :users, skip: [:sessions, :registrations, :passwords]
   as :user do
     get 'admin_login', to: 'devise/sessions#new', as: :new_user_session
@@ -16,6 +10,4 @@ Rails.application.routes.draw do
     delete 'admin_logout', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 
-  # devise_for :users , :skip => 'sign_up'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
