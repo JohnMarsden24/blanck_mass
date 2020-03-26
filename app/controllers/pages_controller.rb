8878ba190441f88ac76f93b21a3dcc6753594c97
John Marsden <john.marsden24@gmail.com>
2020-03-26 15:37:28 +0000

    added post logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,7 @@ class PagesController < ApplicationController
   end
 
   def home
+    @post = Post.new
+    @posts = Post.all
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("trix")
+require("@rails/actiontext")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -22,3 +23,6 @@ require("channels")
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
 import "bootstrap";
+
+document.addEventListener('turbolinks:load', () => {
+});
