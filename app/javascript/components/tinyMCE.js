fae358642ae82a3ffb31db8059d423fa8cf15e7b
John Marsden <john.marsden24@gmail.com>
2020-04-07 17:31:00 +0100

    pushing to heroku

--- app/javascript/components/tinyMCE.js
+++ app/javascript/components/tinyMCE.js
@@ -4,8 +4,8 @@ const postEditor = () => {
   tinymce.remove()
   tinymce.init({
     selector: '.post-form',
-    plugins: 'link a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media permanentpen',
-    toolbar: 'link a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen',
+    plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen',
+    toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen',
     toolbar_mode: 'floating',
     tinycomments_mode: 'embedded',
     tinycomments_author: 'Author name',
